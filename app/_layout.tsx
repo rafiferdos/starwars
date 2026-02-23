089279fbff0dd9e8913c9cc23d22a151e7db0df1
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-23 11:24:30 +0600

    feat: add tab screens for films, people, and favorites with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,7 @@
 import { COLORS } from '@/constants/colors'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { Tabs } from 'expo-router'
 
 export default function RootLayout() {
@@ -17,13 +20,46 @@ export default function RootLayout() {
         tabBarStyle: {
           backgroundColor: COLORS.tabBar,
           borderTopColor: COLORS.dark,
-          borderTopWidth: 1,
+          borderTopWidth: 1
         },
         tabBarActiveTintColor: COLORS.activeTab,
-        tabBarInactiveTintColor: COLORS.inactiveTab,
+        tabBarInactiveTintColor: COLORS.inactiveTab
       }}
     >
       <Tabs.Screen name='index' options={{ href: null }} />
+      <Tabs.Screen
+        name='films'
+        options={{
+          animation: 'fade',
+          title: 'Films & Movies',
+          tabBarLabel: 'Films',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='film' size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='people'
+        options={{
+          animation: 'fade',
+          title: 'All Characters',
+          tabBarLabel: 'People',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name='people-group' size={24} color='black' />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='favorites'
+        options={{
+          animation: 'fade',
+          title: 'Favorites',
+          tabBarLabel: 'Favorites',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name='favorite' size={24} color='black' />
+          )
+        }}
+      />
     </Tabs>
   )
 }

070a9a013e5dba04d9e612e97f50c1ae4abf6022
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-23 10:43:56 +0600

    feat: implement films component and update layout to use Tabs

---
+++ app/films.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const films = () => {
+  return (
+    <View>
+      <Text>films</Text>
+    </View>
+  )
+}
+
+export default films

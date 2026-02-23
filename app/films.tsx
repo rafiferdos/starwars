3a24f55dba1ff264e3b8db4b985763833d9e627f
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-23 12:08:33 +0600

    feat: implement film fetching and display using FlatList component

--- app/films.tsx
+++ app/films.tsx
@@ -1,15 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { IFilms } from '@/types/films'
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
-const films = () => {
+const Page = () => {
+  const [films, setFilms] = useState<IFilms[]>([])
+  const [loading, setLoading] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
+
+  const fetchFilms = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json()
+      console.log('🚀 ~ fetchFilms ~ data:', data)
+      setFilms(data.results)
+    } catch (error) {
+      console.error('Error fetching films:', error)
+    } finally {
+      setLoading(false)
+      setRefreshing(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchFilms()
+  }, [])
+
   return (
-    <View style={styles.containerStyle} >
-      <Text>films</Text>
+    <View style={styles.containerStyle}>
+      <FlatList
+        data={films}
+        keyExtractor={(item) => item.episode_id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 20,
+              padding: 15,
+              backgroundColor: '#3a3a3a',
+              borderRadius: 10
+            }}
+          >
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#fff' }}>
+              {item.title}
+            </Text>
+            <Text style={{ color: '#ccc' }}>Director: {item.director}</Text>
+            <Text style={{ color: '#ccc' }}>
+              Release Date: {item.release_date}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   )
 }
 
-export default films
+export default Page
 
 const styles = StyleSheet.create({
   containerStyle: {

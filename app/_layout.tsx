d090f63fc0e64acc1218257b855d3c0b43f92a88
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-23 11:06:29 +0600

    feat: enhance layout with consistent header styling and update colors constant

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,22 @@
+import { COLORS } from '@/constants/colors'
 import { Tabs } from 'expo-router'
 
 export default function RootLayout() {
-  return <Tabs>
-    <Tabs.Screen name="index" options={{ href: null }} />
-  </Tabs>
+  return (
+    <Tabs
+      screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: COLORS.dark
+        },
+        headerTintColor: COLORS.gold,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}
+    >
+      <Tabs.Screen name='index' options={{ href: null }} />
+    </Tabs>
+  )
 }
--- constants/colors.ts
+++ constants/colors.ts
@@ -1,10 +1,8 @@
-export const colors = {
+export const COLORS = {
   //color dark bg and gold like elegant
   dark: '#1a1a1a',
   gold: '#d4af37',
   white: '#ffffff',
   tabBar: '#333333',
-  text: '#ffffff',
-  
-
+  text: '#ffffff'
 }
